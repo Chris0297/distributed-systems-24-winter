ca9bcd5bbe2d6b582b7aff708f4368e0878ab33a
Michele-92 <130477564+Michele-92@users.noreply.github.com>
2024-11-04 19:53:15 +0000

    Adding POST and PUT Endroute

--- shoppinglist/handler/shopping_handler.go
+++ shoppinglist/handler/shopping_handler.go
@@ -1,11 +1,63 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+var ShoppingList []Items
+
+type Items struct {
+	name string
+	amount int
+}
+
 func GetShoppingItemByName(c *fiber.Ctx) error {
-    //name := c.Params("name")
-	
-    return c.Status(fiber.StatusOK).JSON("Item found and retrieved successfully.")
+	name := c.Params("name")
+	IsValidItem := SearchItem(name)
+	if IsValidItem {
+		return c.Status(fiber.StatusOK).JSON("Item found and retrieved successfully." + name)
+	}
+	return c.Status(fiber.StatusNotFound).JSON("Item not found")
+}
+
+func AddNewShoppingItem(c *fiber.Ctx) error {
+	TestItem := Items{} 
+	name := c.Params("name")
+	TestItem.name = name
+	TestItem.amount = 1
+	ShoppingList = append(ShoppingList,TestItem)
+	OutputShoppinglist()
+	return c.Status(fiber.StatusCreated).JSON("Item successfully created.")
+}
+
+func SearchItem(name string) bool {
+	for _, item := range ShoppingList {
+		if item.name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func UpdateAmount(c *fiber.Ctx) error {
+	name := c.Params("name")
+	ItemCounter(name)
+	OutputShoppinglist()
+	return c.Status(fiber.StatusOK).JSON("Item updated successfully.")
+}
+
+
+func ItemCounter(name string) {
+	for i := range ShoppingList {
+		if ShoppingList[i].name == name {
+			ShoppingList[i].amount++
+		}
+	}
+}
+
+func OutputShoppinglist(){
+	fmt.Printf("%v\n", ShoppingList)
+	fmt.Println(ShoppingList)
 }
--- shoppinglist/main.go
+++ shoppinglist/main.go
@@ -11,6 +11,8 @@ func main() {
 	app := fiber.New()
 	app.Get("/api/shopping/:name", handler.GetShoppingItemByName)
 	fmt.Println("hello")
+	app.Post("/api/shopping/:name", handler.AddNewShoppingItem)
+	app.Put("/api/shopping/:name", handler.UpdateAmount)
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
