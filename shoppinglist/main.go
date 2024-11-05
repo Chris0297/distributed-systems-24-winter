cf464e770eefbfc1555a7650b72cdba057ba6872
Michele-92 <130477564+Michele-92@users.noreply.github.com>
2024-11-05 08:56:43 +0000

    Adding Docker-Compose File

--- shoppinglist/main.go
+++ shoppinglist/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"log"
-	 //"os"
+	"os"
 	"shoppinglist/handler"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -10,10 +10,10 @@ import (
 
 func main() {
 	//Setting port via enviorment variable at the docker run command -e PORT=XXXX
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	log.Fatal("PORT enviorment variable is not set")
-	// }
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT enviorment variable is not set")
+	}
 	apiShoppingWithName := "/api/shopping/:name"
 	
 	app := fiber.New(fiber.Config{
@@ -32,12 +32,12 @@ func main() {
 	app.Post(apiShoppingWithName, handler.AddNewShoppingItem)
 	app.Put(apiShoppingWithName, handler.UpdateItem)
 	app.Delete(apiShoppingWithName, handler.DeleteShoppingItem)
-	// if err := app.Listen(":"+port); err != nil {
-	// 	log.Fatalf("Failed to start server: %v", err)
-	// }
-	if err := app.Listen(":3000"); err != nil {
+	if err := app.Listen(":"+port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+	// if err := app.Listen(":3000"); err != nil {
+	// 	log.Fatalf("Failed to start server: %v", err)
+	// }
 	
 	
 }
